8a6551553f21b723489ca8846cfc97b9ee9f6e81
Mahesh Patil <122906017+05-Mahesh@users.noreply.github.com>
2023-12-21 18:54:26 +0530

    Add files via upload

---
+++ Firstday.R
@@ -0,0 +1,20 @@
+9*7
+name="Mac"
+9a <- 120
+a5_<-505
+a5<- 507
+rm(a5_)
+a <-''
+ a <- 2 
+ b <- 3
+class(a)
+m=""
+n=5
+n <- c(1,2,3,4,5)
+n1 <- c("a",'b','v')
+n2 <- c(10,5.5,TRUE,T,"HI")  #instead of giving error it convert data into string data type
+
+n[1]
+n[1,2] # giving error because its goes to 2D
+n[c(1,2)]  # its a method for 1D to repesent multiple values name as combined method
+
